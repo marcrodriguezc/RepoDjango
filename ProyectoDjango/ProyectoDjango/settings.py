15de2fc8506ba7cb0212ff5650553e59d1c29008
Your Name <marc.rodriguezc@duocuc.cl>
2021-06-03 16:38:12 -0400

    commit

--- ProyectoDjango/ProyectoDjango/settings.py
+++ ProyectoDjango/ProyectoDjango/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #TERCEROS
+    'bootstrap4',
     #DJANGO
     'django.contrib.admin',
     'django.contrib.auth',
--- ProyectoDjango/core/views.py
+++ ProyectoDjango/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'core/index.html')
+
+def contacto(request):
+
+    return render(request, 'core/contacto.html')
